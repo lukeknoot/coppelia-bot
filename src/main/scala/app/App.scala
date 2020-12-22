b6903feea62f35d6450ea4c54583f1538a286c6e
Luke <lukeknoot@gmail.com>
2020-12-22 15:39:22 +1100

    Lower polling frequency

--- src/main/scala/app/App.scala
+++ src/main/scala/app/App.scala
@@ -10,7 +10,7 @@ object App extends zio.App {
   override def run(args: List[String]): URIO[ZEnv, ExitCode] =
     Booking.bookingCycle
       .catchSome({ case e: CoppeliaError => log.error(e.message) })
-      .repeat(Schedule.fixed(1.minute).jittered(1.0, 2.0))
+      .repeat(Schedule.fixed(5.minutes).jittered(1.0, 2.0))
       .provideCustomLayer(AppEnv.live)
       .exitCode
 
