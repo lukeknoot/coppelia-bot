b72336a45d1d99336e695abbe8d3380b6e3d221e
Luke Knight <luke@lukejk.com>
2020-12-26 21:30:50 +1100

    Upgrade sttp

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Version {
   val zio        = "1.0.3"
   val circe      = "0.12.3"
-  val sttp       = "3.0.0-RC11"
+  val sttp       = "3.0.0-RC13"
   val pureConfig = "0.14.0"
   val jsoup      = "1.13.1"
   val zioLogging = "0.4.0"
